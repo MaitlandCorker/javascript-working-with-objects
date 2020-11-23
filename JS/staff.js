ccf84bfa05cff468925145f842e29d0b53adcf6e
Maitland Corker <m.corker@LIVE.CO.UK>
2020-11-23 09:38:31 +0000

    removed

--- JS/staff.js
+++ JS/staff.js
@@ -1,43 +1,6 @@
 (function(){
 //Start
 
-console.info("Hello")
 
-var myObject = { 
-    name : "Maitland", 
-    surname : "Corker", 
-    email : "mcorker7@gmail.co.uk", 
-    qualifications :{
-        gcse : true, 
-        ALevels : true , 
-        beng : false},
-
-getInfo : function(){
-    return this.name + " " + this.surname
-    }
-}
-var qualifications = [
-    {
-    subject : "Maths",
-    grade : "A"
-    },
-    {
-    subject : "English",
-    grade : "B"
-    },
-    {
-    subject : "French",
-    grade : "C"
-    },
-    {
-    subject : "Physics",
-    grade : "B"
-    }
-    ];
-
-console.info(qualifications[1].subject)
-
-console.dir(myObject)
-console.info(myObject.getInfo())
 //End
 })
