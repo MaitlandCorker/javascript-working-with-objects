f437b5dd9a4d8c091c528b6fbdc207af1ad8f260
Maitland Corker <m.corker@LIVE.CO.UK>
2020-11-23 09:28:51 +0000

    format

--- JS/staff.js
+++ JS/staff.js
@@ -3,11 +3,20 @@
 
 console.info("Hello")
 
-var myObject = { name : "Maitland", surname : "Corker", email : "mcorker7@gmail.co.uk", qualifications :{gcse : true, ALevels : true , beng : false},
+var myObject = { 
+    name : "Maitland", 
+    surname : "Corker", 
+    email : "mcorker7@gmail.co.uk", 
+    qualifications :{
+        gcse : true, 
+        ALevels : true , 
+        beng : false},
+
 getInfo : function(){
     return this.name + " " + this.surname
+    }
 }
-}
+
 console.dir(myObject)
 console.info(myObject.getInfo())
 //End
